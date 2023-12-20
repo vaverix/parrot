50e1f29353862500b369d3230b99a677741901c5
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2023-12-20 22:01:38 +0700

    feat(repeatqueue): reset stored_store on leave command

--- src/commands/leave.rs
+++ src/commands/leave.rs
@@ -1,4 +1,7 @@
-use crate::{errors::ParrotError, messaging::message::ParrotMessage, utils::create_response};
+use crate::{
+    errors::ParrotError, guild::stored_queue::GuildStoredQueueMap,
+    messaging::message::ParrotMessage, utils::create_response,
+};
 use serenity::{
     client::Context,
     model::application::interaction::application_command::ApplicationCommandInteraction,
@@ -10,6 +13,19 @@ pub async fn leave(
 ) -> Result<(), ParrotError> {
     let guild_id = interaction.guild_id.unwrap();
     let manager = songbird::get(ctx).await.unwrap();
+    let mut data = ctx.data.write().await;
+    match data.get_mut::<GuildStoredQueueMap>() {
+        Some(stored_queue) => match stored_queue.get_mut(&guild_id) {
+            Some(guild_stored_queue) => {
+                guild_stored_queue.queue.clear();
+            }
+            None => (),
+        },
+        _ => (),
+    }
+
+    drop(data);
+
     manager.remove(guild_id).await.unwrap();
 
     create_response(&ctx.http, interaction, ParrotMessage::Leaving).await
