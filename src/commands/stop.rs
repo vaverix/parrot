80592ef8c5b171111258a6049e7d326d03664bc1
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2023-12-20 22:01:07 +0700

    feat(repeatqueue): add reset stored_queue on stop command

--- src/commands/stop.rs
+++ src/commands/stop.rs
@@ -1,5 +1,6 @@
 use crate::{
     errors::{verify, ParrotError},
+    guild::stored_queue::GuildStoredQueueMap,
     handlers::track_end::update_queue_messages,
     messaging::message::ParrotMessage,
     utils::create_response,
@@ -16,6 +17,13 @@ pub async fn stop(
     let guild_id = interaction.guild_id.unwrap();
     let manager = songbird::get(ctx).await.unwrap();
     let call = manager.get(guild_id).unwrap();
+
+    let mut data = ctx.data.write().await;
+    let stored_queue = data.get_mut::<GuildStoredQueueMap>().unwrap();
+    let guild_stored_queue = stored_queue.get_mut(&guild_id).unwrap();
+    guild_stored_queue.continue_play = false;
+    guild_stored_queue.queue.clear();
+    drop(data);
 
     let handler = call.lock().await;
     let queue = handler.queue();
